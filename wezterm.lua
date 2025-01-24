eb02ccf763f9473bdea7808e8df18c6182fc2ff8
holos <holosnikitatt@gmail.com>
2025-01-24 09:31:21 +0200

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -2,12 +2,12 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-
 config.color_scheme = 'Afterglow'
 config.font = wezterm.font 'JetBrains Mono'
 config.font_size = 14.0
 config.default_cursor_style = 'BlinkingBar'
 config.enable_wayland = true
 config.hide_tab_bar_if_only_one_tab = true
+config.warn_about_missing_glyphs = false
 
 return config
