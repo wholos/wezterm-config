e69536b00a46af13821f2d7544a52540ad7f0fff
holos <holosnikitatt@gmail.com>
2025-02-11 14:25:31 +0200

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Afterglow'
+config.color_scheme = 'Breath Silverfox (Gogh)'
 config.font = wezterm.font 'JetBrains Mono'
 config.font_size = 14.0
 config.default_cursor_style = 'BlinkingBar'
