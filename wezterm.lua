b39e83c730b13de30f1b16c58d9e0cd31f6649b8
holos <holosnikitatt@gmail.com>
2025-02-16 14:30:02 +0200

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -1,3 +1,5 @@
+-- Wezterm config
+-- https://github.com/wholos
 local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
@@ -6,7 +8,7 @@ config.color_scheme = 'Breath Silverfox (Gogh)'
 config.font = wezterm.font 'JetBrains Mono'
 config.font_size = 14.0
 config.default_cursor_style = 'BlinkingBar'
-config.enable_wayland = true
+config.enable_wayland = false -- Change True or False
 config.hide_tab_bar_if_only_one_tab = true
 config.warn_about_missing_glyphs = false
 
